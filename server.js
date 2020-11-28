869ec6071007284b3d3fa8f6a2034ea8f7a9af75
Joe Young <musicjoeyoung@gmail.com>
2020-11-28 15:23:40 -0500

    update 4

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const app = express();
 const morgan = require("morgan");
 const host = "127.0.0.1";
 const port = 3000;
+const bodyParser = require("body-parser");
+const { Liquid } = require("liquidjs");
+const engine = new Liquid({
+  root: [".", "./views/layouts"], // where the engine will look for .liquid files
+  extname: ".liquid", //now we don't have to write .liquid on every while
+});
+app.engine("liquid", engine.express());
+app.use(bodyParser.urlencoded({ extended: true })); //to handle form parsing
+app.set("views", "./views");//set the directory where app will look for html/view
+app.set("view engine", "liquid");//set the view engine
+
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
@@ -11,6 +22,10 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
   res.send(`<h1>Login</h1>`);
+});
+
+app.get("/users/account", (req, res) => {
+  res.send(`<h1>Account settings </h1>`);
 });
 
 app.get("/events/all", (req, res) => {
